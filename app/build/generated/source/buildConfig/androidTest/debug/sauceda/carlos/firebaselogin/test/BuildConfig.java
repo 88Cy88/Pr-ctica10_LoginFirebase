407972ddcba78c310d7a6cf3448f6c680bef7e2d
88Cy88 <carlos.sauceda.bot@gmail.com>
2023-04-21 14:28:25 -0700

    se agrego apk

---
+++ app/build/generated/source/buildConfig/androidTest/debug/sauceda/carlos/firebaselogin/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package sauceda.carlos.firebaselogin.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "sauceda.carlos.firebaselogin.test";
+  public static final String BUILD_TYPE = "debug";
+}
